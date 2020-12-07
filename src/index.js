a2f54c10d844567ba8e71616a5b5b60bba806e11
pikokr <parangee9706@gmail.com>
2020-12-07 21:49:58 +0900

    basic graphql server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hello world!')
+require('graphql-import-node')
+const mongoose = require('mongoose')
+
+const { ApolloServer } = require('apollo-server-express')
+
+const typeDefs = require('./schema.graphql')
+
+const resolvers = require('./resolvers')
+
+const express = require('express')
+
+const config = require('../config.json')
+
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+const apollo = new ApolloServer({typeDefs, resolvers})
+
+apollo.applyMiddleware({app})
+
+
+mongoose.connect(config.database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(1234, () => console.log('Listening')))
