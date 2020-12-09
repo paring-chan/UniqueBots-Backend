911880c3e8c54c3172ae98a680f2c29f42c05fd0
pikokr <parangee9706@gmail.com>
2020-12-09 16:30:08 +0900

    feat: add bot

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,6 +1,7 @@
+const {ApolloError} = require("apollo-server-errors");
 module.exports = (fn) => {
     return (...params) => {
-        if (!params[2].user) return null
+        if (!params[2].user) throw new ApolloError('Login required', 'LOGIN_REQUIRED')
         return fn(...params)
     }
 }
--- src/resolvers/Mutation/addBot.js
+++ src/resolvers/Mutation/addBot.js
@@ -1,5 +1,45 @@
 const loginRequired = require('../../middlewares/loginRequired')
+const Bot = require("../../models/Bot");
+const Judge = require("../../models/Judge");
+const {ApolloError} = require("apollo-server-errors");
 
-module.exports = loginRequired((parent, args, ctx) => {
-    console.log(args)
+
+module.exports = loginRequired(async (parent, args, ctx) => {
+    if (!args.id) throw new ApolloError('id Field required', 'ID_REQUIRED', {
+        parameter: 'id'
+    })
+    if (!args.brief) throw new ApolloError('brief Field required', 'BRIEF_REQUIRED', {
+        parameter: 'brier'
+    })
+    if (!args.description) throw new ApolloError('description Field required', 'DESCRIPTION_REQUIRED', {
+        parameter: 'description'
+    })
+    if (args.id.length < 17 || args.id.length > 19 || isNaN(Number(args.id))) {
+        throw new ApolloError('아이디의 길이는 17-19 사이어야 하고 숫자여야 합니다.')
+    }
+
+    if (await Judge.findOne({requester: ctx.user.meta.id, pending: true})) {
+        throw new ApolloError('이미 진행중인 심사가 있네요!', 'ERR_JUDGING_OTHER')
+    }
+
+    if (await Bot.findOne({id: args.id})) {
+        throw new ApolloError('해당 ID의 봇이 이미 존재합니다.', 'ERR_BOT_ALREADY_EXISTS', {parameter: 'id'})
+    }
+
+    const judge = new Judge()
+
+    judge.id = args.id
+    judge.requester = ctx.user.meta.id
+
+    await judge.save()
+
+    const bot = new Bot()
+
+    bot.id = args.id
+    bot.description = args.description.slice(0,1500)
+    bot.brief = args.brief.slice(0,50)
+
+    await bot.save()
+
+    return true
 })
