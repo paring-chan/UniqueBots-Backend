8d9ab4d6296e5e41235751b68896e56b63ef4882
pikokr <parangee9706@gmail.com>
2020-12-10 20:31:41 +0900

    feat: verify if client is bot

--- src/resolvers/Mutation/addBot.js
+++ src/resolvers/Mutation/addBot.js
@@ -2,6 +2,7 @@ const loginRequired = require('../../middlewares/loginRequired')
 const Bot = require("../../models/Bot");
 const Judge = require("../../models/Judge");
 const {ApolloError} = require("apollo-server-errors");
+const {evaluate} = require('../../util/bot')
 
 
 module.exports = loginRequired(async (parent, args, ctx) => {
@@ -16,6 +17,12 @@ module.exports = loginRequired(async (parent, args, ctx) => {
     })
     if (args.id.length < 17 || args.id.length > 19 || isNaN(Number(args.id))) {
         throw new ApolloError('아이디의 길이는 17-19 사이어야 하고 숫자여야 합니다.')
+    }
+
+    if (!await evaluate(`
+        client.users.fetch(${args.id}).then(res => res.bot).catch(e => false)
+    `)) {
+        throw new ApolloError('입력한 id에 일치하는 봇을 찾을 수 없어요!', 'ERR_INVALID_CLIENT_ID')
     }
 
     if (await Judge.findOne({requester: ctx.user.meta.id, pending: true})) {
